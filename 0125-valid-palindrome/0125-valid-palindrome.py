ed4cdb6eea1fce981e54b796d0677605817f6d1e
harshithak1805 <harshithak1805@gmail.com>
2026-03-19 21:11:13 +0530

    Time: 10 ms (38.12%), Space: 23.9 MB (5.46%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res=[]
+        for i in s:
+            if i.isalnum():
+                res.append(i.lower())
+        if res==res[::-1]:
+            return True
+        else:
+            return False
